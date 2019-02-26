e7b6f8f8d304411d9962c7529a6b01b3065449dc
Stefan <psn237@nyu.edu>
2019-02-26 22:23:51 +0400

    LS and PWD for client

--- FTPclient.c
+++ FTPclient.c
@@ -1,15 +1,15 @@
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <stdlib.h>
 #include <errno.h>
-#include <stdio.h>
 #include <stdbool.h>
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
 #include <sys/time.h>
 #include <sys/types.h>
+#include <netinet/in.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(int argc, char * argv[])
 {
@@ -21,7 +21,7 @@ int main(int argc, char * argv[])
   }
 
   // define server details
-  const char * const server_address = argv[1]; // CAN'T CHANGE server_address
+  const char * const server_address = argv[1]; // NOTE: this doesn't do anything
   int server_port = atoi(argv[2]);
   int sockfd, i;
   struct sockaddr_in addr;
@@ -43,14 +43,16 @@ int main(int argc, char * argv[])
   }
 
   while (true) {
+    char input[1024];
+    bool validInput = false;
+    
     printf("ftp > ");
     fgets(buf, sizeof(buf), stdin);
-    char input[1024];
     strncpy (input, buf, sizeof(buf));
-    bool validInput = false;
+    
     char * command = strtok(buf, " \n");
     char * arg1 = strtok(NULL, " ");
-    printf("%s\n", input);
+    // printf("%s\n", input); // uncomment to view input
     if (!strcmp(command, "USER")) {
       printf("Entered USERNAME\n");
       if (!arg1) {  // No username provided
@@ -70,19 +72,37 @@ int main(int argc, char * argv[])
     } else if (!strcmp(command, "PUT")) {
       printf("Entered PUT\n");
       if (!arg1) {  // No filename provided
-        printf("Please input filename.\n");
+        printf("Please input filename from client.\n");
       } else {
         validInput = true;
         write(sockfd, input, strlen(input+1));
       }
     } else if (!strcmp(command, "GET")) {
+      printf("Entered GET\n");
       if (!arg1) {  // No filename provided
-        printf("Please input filename.\n");
+        printf("Please input filename from host.\n");
       } else {
         validInput = true;
         write(sockfd, input, strlen(input+1));
       }
-      printf("Entered GET\n");
+    } else if (!strcmp(command, "CD") || !strcmp(command, "LS") || !strcmp(command, "PWD")) {
+      validInput = true;
+      write(sockfd, input, strlen(input+1));
+    } else if (!strcmp(command, "!LS") || !strcmp(command, "!PWD")) {
+      int status;
+      char *args[2];
+      if (!strcmp(command, "!LS")) 
+        args[0] = "/bin/ls";        // first arg is the full path to the executable
+      else
+        args[0] = "/bin/pwd";        // first arg is the full path to the executable
+      args[1] = NULL;             // list of args must be NULL terminated
+      if (fork() == 0) {
+        execv( args[0], args ); // child: call execv with the path and the args
+      } else {
+        wait( &status );        // parent: wait for the child (not really necessary)
+      }
+    } else if (!strcmp(command, "QUIT")) {
+      exit(1);
     }
     if (validInput) {
       if (read(sockfd, input, sizeof(input)) == 0) {
