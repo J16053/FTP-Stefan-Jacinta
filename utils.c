dbad929c0c44c32982a247e6c0479e55a6ba2bc6
Stefan <psn237@nyu.edu>
2019-03-03 14:01:27 +0400

    Check Point I

--- utils.c
+++ utils.c
@@ -5,15 +5,15 @@ int changeDir(const char *input) {
   char command[num_chars];
   strncpy(command, input, num_chars);
   strtok(command, "\n");
-  const char * sep = strchr(command, ' ');
-  const char * path;
+  const char *sep = strchr(command, ' ');
+  const char *path;
   if (!sep) {
     path = "/home";   // default to home directory
   } else {
     path = sep + 1;
   }
   if (chdir(path) == -1) {
-    printf("Error: %s\n", strerror(errno));
+    perror("Error: ");
     return EXIT_FAILURE;
   } else {
     return EXIT_SUCCESS;
